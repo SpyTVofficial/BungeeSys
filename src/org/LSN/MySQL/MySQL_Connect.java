014373d0315cab6dde42df0a820c73d2460691f3
EyeDe7 <noa.bamberg@protonmail.com>
2022-11-03 22:53:09 +0100

    deng mamm ass fett

--- src/org/LSN/MySQL/MySQL_Connect.java
+++ src/org/LSN/MySQL/MySQL_Connect.java
@@ -4,6 +4,7 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 import java.sql.*;
+import java.text.MessageFormat;
 import java.util.UUID;
 
 public class MySQL_Connect {
@@ -35,7 +36,7 @@ public class MySQL_Connect {
             try {
                 con.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS `banned` ( `id` INT(11) NOT NULL AUTO_INCREMENT ,`name` VARCHAR(100) NOT NULL, `UUID` VARCHAR(100) NOT NULL , `reason` VARCHAR(100) NOT NULL , `ende` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;");
                 con.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS `muted` ( `id` INT(11) NOT NULL AUTO_INCREMENT ,`name` VARCHAR(100) NOT NULL, `UUID` VARCHAR(100) NOT NULL , `reason` VARCHAR(100) NOT NULL , `ende` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;");
-                con.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS `users` ( `id` INT(11) NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL, `uuid` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;");
+                con.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS `users` ( `12id` INT(11) NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL, `uuid` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;");
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -45,7 +46,7 @@ public class MySQL_Connect {
         return (con != null);
     }
 
-    public static void update(String query, String s){
+    public static void update(String query){
         if(isConnected())
             try{
                 con.createStatement().executeUpdate(query);
@@ -68,7 +69,8 @@ public class MySQL_Connect {
 
     public static boolean ifPlayerExist(String uuid){
         try{
-            ResultSet r = query("SELECT * FROM users WHERE UUID= '" + uuid + "'");
+            String sql = MessageFormat.format("SELECT NAME FROM users WHERE UUID= {0}", uuid);
+            ResultSet r = query(sql);
             if(r.next())
                 return (r.getString("UUID") != null);
             return false;
@@ -80,8 +82,9 @@ public class MySQL_Connect {
 
     public static void createPlayer(String name, String uuid, ProxiedPlayer p){
         UUID uniqueId = p.getUniqueId();
+        String query = MessageFormat.format("INSERT INTO users(NAME, UUID) VALUES ({0}, {1})", name, uniqueId);
         if(!ifPlayerExist(uuid)){
-            update("INSERT INTO users(ID, NAME, UUID) VALUES ('","', '" + ProxyServer.getInstance().getPlayer(UUID.fromString(uuid)).getDisplayName() + "', '" + uniqueId +"'");
+            update(query);
         }
     }
 }
